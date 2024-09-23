b5d7ddc562e34f7c691dc590904db591f4df2c81
Jonatan <jonatan.vonmartens@gmail.com>
2024-09-23 20:52:09 +0300

    impr: set new bg color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased dark:bg-zinc-950`}
       >
         <ThemeProvider
           attribute="class"
