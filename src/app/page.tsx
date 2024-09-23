f078ded353a4db1ce67b66a4c23054f1a7c86052
Jonatan <jonatan.vonmartens@gmail.com>
2024-09-23 19:43:43 +0300

    Inital

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,100 +1,75 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { PropsWithChildren } from "react";
+import { cn } from "@/lib/utils";
+import { ModeToggle } from "@/components/mode-toggle";
+import { Card, CardContent } from "@/components/ui/card";
+import { LinkedInIcon } from "@/components/icons/linkedIn-icon";
+import { GithubIcon } from "@/components/icons/github-icon";
+import { TwitterIcon } from "@/components/icons/twitter-icon";
+
+function SocialLink({ href, children }: PropsWithChildren<{ href: string }>) {
+  return (
+    <Link href={href} target={"_blank"} passHref>
+      <Button variant={"outline"} size={"lg"} className={"w-full"}>
+        {children}
+      </Button>
+    </Link>
+  );
+}
+
+function Me({ className }: { className?: string }) {
+  return (
+    <Card className={cn(className)}>
+      <CardContent className="p-0 cursor-auto">
+        <div className="flex items-center p-4 gap-x-4">
+          <div className="flex-shrink-0">
+            <img
+              alt=""
+              src={"/profile.png"}
+              className="h-10 w-10 rounded-full"
+            />
+          </div>
+          <div className="min-w-0 flex-1">
+            <p className="text-sm font-medium">Jonatan von Martens</p>
+            <p className="truncate text-sm">@jonatanvm</p>
+          </div>
+          <div>
+            <ModeToggle />
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start max-w-sm w-full">
+        <div className="flex gap-4 items-center flex-col w-full">
+          <Me className={"w-full"} />
+          <ul className={"flex flex-col w-full gap-y-3"}>
+            <li className={"w-full"}>
+              <SocialLink href={"https://www.linkedin.com/in/jonatanvm/"}>
+                <LinkedInIcon className="h-5 w-5" aria-label="LinkedIn" />
+              </SocialLink>
+            </li>
+            <li>
+              <SocialLink href={"https://github.com/jonatanvm"}>
+                <GithubIcon className="h-5 w-5" aria-label={"Github"} />
+              </SocialLink>
+            </li>
+            <li>
+              <SocialLink href={"https://x.com/_jonatanvm"}>
+                <TwitterIcon className="h-5 w-5" aria-label={"X.com"} />
+              </SocialLink>
+            </li>
+          </ul>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center text-zinc-300 dark:text-zinc-800">
+        © jonatanvm.com
       </footer>
     </div>
   );
