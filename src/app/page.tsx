5dd16c79e2842fd72f7dfd0808078c54995bbfed
Jonatan <jonatan.vonmartens@gmail.com>
2024-09-26 00:14:02 +0300

    feat: add timeline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,20 @@ import Link from "next/link";
 import { PropsWithChildren } from "react";
 import { cn } from "@/lib/utils";
 import { ModeToggle } from "@/components/mode-toggle";
-import { Card, CardContent } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { LinkedInIcon } from "@/components/icons/linkedIn-icon";
 import { GithubIcon } from "@/components/icons/github-icon";
 import { TwitterIcon } from "@/components/icons/twitter-icon";
 import Image from "next/image";
-import { MailIcon } from "lucide-react";
+import { CircleDot, MailIcon } from "lucide-react";
+import {
+  Timeline,
+  TimelineContent,
+  TimelineDot,
+  TimelineHeading,
+  TimelineItem,
+  TimelineLine,
+} from "@/components/ui/timeline";
 
 function SocialLink({ href, children }: PropsWithChildren<{ href: string }>) {
   return (
@@ -36,8 +44,8 @@ function Me({ className }: { className?: string }) {
             />
           </div>
           <div className="min-w-0 flex-1">
-            <p className="text-sm font-medium">Jonatan von Martens</p>
-            <p className="truncate text-sm">@jonatanvm</p>
+            <p className="sm:text-sm font-medium">Jonatan von Martens</p>
+            <p className="truncate sm:text-sm">@jonatanvm</p>
           </div>
           <div>
             <ModeToggle />
@@ -49,33 +57,106 @@ function Me({ className }: { className?: string }) {
 }
 
 export default function Home() {
+  const jobs = [
+    {
+      employer: "Elevenlabs",
+      title: "Software Engineer",
+      time: "Aug 2024 - Present",
+    },
+    {
+      employer: "Root Signals",
+      title: "Lead Software Engineer",
+      time: "Apr 2024 - Aug 2024",
+    },
+    {
+      employer: "Mobal",
+      title: "Head of Product and Technology",
+      time: "Jul 2021 - Aug 2023",
+    },
+    {
+      employer: "RhombIT Consulting",
+      title: "Software Engineer",
+      time: "Jun 2019 - Jul 2021",
+    },
+    {
+      employer: "SmartValley Wearable Oy",
+      title: "CTO",
+      time: "May 2017 - Aug 2020",
+    },
+    {
+      employer: "jara.ai",
+      title: "Co-founder",
+      time: "Oct 2018 - Aug 2019",
+    },
+  ];
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-4 sm:p-20 pb-20 gap-16 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start max-w-sm w-full">
-        <div className="flex gap-4 items-center flex-col w-full">
-          <Me className={"w-full"} />
-          <ul className={"flex flex-col w-full gap-y-3"}>
-            <li className={"w-full"}>
-              <SocialLink href={"https://www.linkedin.com/in/jonatanvm/"}>
-                <LinkedInIcon className="h-5 w-5" aria-label="LinkedIn" />
-              </SocialLink>
-            </li>
-            <li>
-              <SocialLink href={"https://github.com/jonatanvm"}>
-                <GithubIcon className="h-5 w-5" aria-label={"Github"} />
-              </SocialLink>
-            </li>
-            <li>
-              <SocialLink href={"https://x.com/_jonatanvm"}>
-                <TwitterIcon className="h-5 w-5" aria-label={"X.com"} />
-              </SocialLink>
-            </li>
-            <li>
-              <SocialLink href={"mailto:Jonatan <stepped_jays.0m@icloud.com>"}>
-                <MailIcon className="h-5 w-5" aria-label={"X.com"} />
-              </SocialLink>
-            </li>
-          </ul>
+    <div className="grid items-center justify-items-center p-4 sm:p-20 pb-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full">
+        <div className="grid grid-cols-1 sm:grid-cols-1 gap-4 w-full">
+          <div className="flex flex-col gap-4 w-full sm:max-w-sm mx-auto">
+            <Me className={"w-full"} />
+            <ul className={"grid grid-cols-4 sm:grid-cols-4 w-full gap-3"}>
+              <li className={"w-full"}>
+                <SocialLink href={"https://www.linkedin.com/in/jonatanvm/"}>
+                  <LinkedInIcon
+                    className="h-5 w-5 shrink-0"
+                    aria-label="LinkedIn"
+                  />
+                </SocialLink>
+              </li>
+              <li>
+                <SocialLink href={"https://github.com/jonatanvm"}>
+                  <GithubIcon
+                    className="h-5 w-5 shrink-0"
+                    aria-label={"Github"}
+                  />
+                </SocialLink>
+              </li>
+              <li>
+                <SocialLink href={"https://x.com/_jonatanvm"}>
+                  <TwitterIcon
+                    className="h-5 w-5 shrink-0"
+                    aria-label={"X.com"}
+                  />
+                </SocialLink>
+              </li>
+              <li>
+                <SocialLink
+                  href={"mailto:Jonatan <stepped_jays.0m@icloud.com>"}
+                >
+                  <MailIcon className="h-5 w-5 shrink-0" aria-label={"X.com"} />
+                </SocialLink>
+              </li>
+            </ul>
+          </div>
+          <Card className={"w-full sm:max-w-sm mx-auto"}>
+            <CardHeader>
+              <CardTitle>Experience</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <Timeline className={"gap-y-4 mr-auto"}>
+                {jobs.map((job) => (
+                  <TimelineItem status="done" key={job.time}>
+                    <TimelineHeading className={"sm:text-sm"}>
+                      {job.employer}
+                    </TimelineHeading>
+                    <TimelineDot className="size-3">
+                      <CircleDot />
+                    </TimelineDot>
+                    <TimelineLine className={"py-1"} done />
+                    <TimelineContent className={"sm:text-sm flex flex-col"}>
+                      <span className={"dark:text-zinc-300 text-zinc-700"}>
+                        {job.title}
+                      </span>
+                      <span className={"dark:text-zinc-500 text-zinc-900"}>
+                        {job.time}
+                      </span>
+                    </TimelineContent>
+                  </TimelineItem>
+                ))}
+              </Timeline>
+            </CardContent>
+          </Card>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center text-zinc-300 dark:text-zinc-800 hidden">
---
+++ src/components/ui/timeline.tsx
@@ -0,0 +1,189 @@
+import React from "react";
+import { VariantProps, cva } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+import { CheckIcon, CrossIcon, DotIcon } from "lucide-react";
+
+const timelineVariants = cva("grid", {
+  variants: {
+    positions: {
+      left: "[&>li]:grid-cols-[0_min-content_1fr]",
+      right: "[&>li]:grid-cols-[1fr_min-content]",
+      center: "[&>li]:grid-cols-[1fr_min-content_1fr]",
+    },
+  },
+  defaultVariants: {
+    positions: "left",
+  },
+});
+
+interface TimelineProps
+  extends React.HTMLAttributes<HTMLUListElement>,
+    VariantProps<typeof timelineVariants> {}
+
+const Timeline = React.forwardRef<HTMLUListElement, TimelineProps>(
+  ({ children, className, positions, ...props }, ref) => {
+    return (
+      <ul
+        className={cn(timelineVariants({ positions }), className)}
+        ref={ref}
+        {...props}
+      >
+        {children}
+      </ul>
+    );
+  },
+);
+Timeline.displayName = "Timeline";
+
+const timelineItemVariants = cva("grid items-center gap-x-2", {
+  variants: {
+    status: {
+      done: "text-primary",
+      default: "text-muted-foreground",
+    },
+  },
+  defaultVariants: {
+    status: "default",
+  },
+});
+
+interface TimelineItemProps
+  extends React.HTMLAttributes<HTMLLIElement>,
+    VariantProps<typeof timelineItemVariants> {}
+
+const TimelineItem = React.forwardRef<HTMLLIElement, TimelineItemProps>(
+  ({ className, status, ...props }, ref) => (
+    <li
+      className={cn(timelineItemVariants({ status }), className)}
+      ref={ref}
+      {...props}
+    />
+  ),
+);
+TimelineItem.displayName = "TimelineItem";
+
+type TimelineDotProps = React.HTMLAttributes<HTMLDivElement>;
+
+const TimelineDot = React.forwardRef<HTMLDivElement, TimelineDotProps>(
+  ({ className, children, ...props }, ref) => (
+    <div
+      role="status"
+      className={cn(
+        "col-start-1 col-end-3 row-start-1 row-end-1 flex size-4 items-center justify-center",
+        className,
+      )}
+      ref={ref}
+      {...props}
+    >
+      {children}
+    </div>
+  ),
+);
+TimelineDot.displayName = "TimelineDot";
+
+const timelineContentVariants = cva(
+  "row-start-2 row-end-2 text-muted-foreground",
+  {
+    variants: {
+      side: {
+        right: "col-start-3 col-end-4 mr-auto text-left",
+        left: "col-start-1 col-end-2 ml-auto text-right",
+      },
+    },
+    defaultVariants: {
+      side: "right",
+    },
+  },
+);
+
+interface TimelineConentProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof timelineContentVariants> {}
+
+const TimelineContent = React.forwardRef<HTMLDivElement, TimelineConentProps>(
+  ({ className, side, ...props }, ref) => (
+    <div
+      className={cn(timelineContentVariants({ side }), className)}
+      ref={ref}
+      {...props}
+    />
+  ),
+);
+TimelineContent.displayName = "TimelineContent";
+
+const timelineHeadingVariants = cva(
+  "row-start-1 row-end-1 line-clamp-1 max-w-full truncate",
+  {
+    variants: {
+      side: {
+        right: "col-start-3 col-end-4 mr-auto text-left",
+        left: "col-start-1 col-end-2 ml-auto text-right",
+      },
+      variant: {
+        primary: "text-base font-medium text-primary",
+        secondary: "text-sm font-light text-muted-foreground",
+      },
+    },
+    defaultVariants: {
+      side: "right",
+      variant: "primary",
+    },
+  },
+);
+
+interface TimelineHeadingProps
+  extends React.HTMLAttributes<HTMLParagraphElement>,
+    VariantProps<typeof timelineHeadingVariants> {}
+
+const TimelineHeading = React.forwardRef<
+  HTMLParagraphElement,
+  TimelineHeadingProps
+>(({ className, side, variant, ...props }, ref) => (
+  <p
+    role="heading"
+    aria-level={variant === "primary" ? 2 : 3}
+    className={cn(timelineHeadingVariants({ side, variant }), className)}
+    ref={ref}
+    {...props}
+  />
+));
+TimelineHeading.displayName = "TimelineHeading";
+
+interface TimelineLineProps extends React.HTMLAttributes<HTMLHRElement> {
+  done?: boolean;
+}
+
+const TimelineLine = React.forwardRef<HTMLHRElement, TimelineLineProps>(
+  ({ className, done = false, ...props }, ref) => {
+    return (
+      <div
+        role="separator"
+        aria-orientation="vertical"
+        className={cn(
+          "col-start-1 col-end-3 row-start-2 row-end-2 mx-auto flex h-full rounded-full align-top justify-start",
+          className,
+        )}
+        ref={ref}
+        {...props}
+      >
+        <hr
+          className={cn(
+            "h-full w-[1px]",
+            done ? "bg-black dark:bg-zinc-50" : "bg-zinc-500 dark:bg-zinc-900",
+          )}
+        />
+      </div>
+    );
+  },
+);
+TimelineLine.displayName = "TimelineLine";
+
+export {
+  Timeline,
+  TimelineDot,
+  TimelineItem,
+  TimelineContent,
+  TimelineHeading,
+  TimelineLine,
+};
